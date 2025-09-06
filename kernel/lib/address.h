ff740bd39c31bcc1fe7d7e3ef9f0e457baa08744
devreizero <gmreizero@gmail.com>
2025-09-06 12:00:33 +0700

    added: more address types
    - Added type `Address`
    - Added 8-bit to 64-bit variants to type `Offset`
    - Added 8-bit to 64-bit variants to type `Index`
    - Added type `Limit` with 8-bit to 64-bit variants
    - Added type `Reserved` with 8-bit to 64-bit variants

--- kernel/lib/address.h
+++ kernel/lib/address.h
@@ -4,10 +4,31 @@
 #include <stdint.h>
 #include <stddef.h>
 
+typedef uintptr_t Address;
 typedef uintptr_t VirtAddr;
 typedef uintptr_t PhysAddr;
+
 typedef uintptr_t Offset;
+typedef uint64_t Offset64;
+typedef uint32_t Offset32;
+typedef uint16_t Offset16;
+typedef uint8_t Offset8;
 
 typedef size_t Index;
+typedef uint64_t Index64;
+typedef uint32_t Index32;
+typedef uint16_t Index16;
+typedef uint8_t Index8;
+
+typedef size_t Limit;
+typedef uint64_t Limit64;
+typedef uint32_t Limit32;
+typedef uint16_t Limit16;
+typedef uint8_t Limit8;
+
+typedef uint64_t Reserved64;
+typedef uint32_t Reserved32;
+typedef uint16_t Reserved16;
+typedef uint8_t Reserved8;
 
 #endif
